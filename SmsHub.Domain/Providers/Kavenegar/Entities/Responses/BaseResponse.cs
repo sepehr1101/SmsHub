b86f9845635732c4b4cfcfdb10a2a1425113f54d
sepehr shamsaie <mantera.sh@hotmail.com>
2024-08-20 10:45:36 +0330

    base request and response kavenegar

---
+++ SmsHub.Domain/Providers/Kavenegar/Entities/Responses/BaseResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsHub.Domain.Providers.Kavenegar.Entities.Responses
+{
+    public class BaseResponse
+    {
+    }
+}
