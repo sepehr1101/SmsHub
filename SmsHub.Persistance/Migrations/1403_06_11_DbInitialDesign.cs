50203a08ff98c937013cfbab83b84ffb7605b9a2
sepehr shamsaie <mantera.sh@hotmail.com>
2024-09-03 10:24:10 +0330

    added db

---
+++ SmsHub.Persistance/Migrations/1403_06_11_DbInitialDesign.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace SmsHub.Persistence.Migrations
+{
+    [Migration(1403061101)]
+    internal class DbInitialDesign:Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Log")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Text").AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Log");
+        }
+    }
+}
