dfef6741336a768e48b3d9d15492e3353eca37be
sepehr shamsaie <mantera.sh@hotmail.com>
2024-09-09 12:24:13 +0330

    missed packages

--- SmsHub.Persistance/Contexts/Implementation/TestContext.cs
+++ SmsHub.Persistance/Contexts/Implementation/TestContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 using SmsHub.Persistence.Models;
 
 namespace SmsHub.Persistence.Contexts.Implementation
