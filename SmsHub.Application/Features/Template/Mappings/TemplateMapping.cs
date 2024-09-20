c743e5fbcca05a304201dc9330f83422507c44b2
ZEtehadi <zetehadi82@gmail.com>
2024-09-20 15:53:50 +0330

    MediatorDto,Add Method,Mapping,Add Handler for Template

---
+++ SmsHub.Application/Features/Template/Mappings/TemplateMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Entities= SmsHub.Domain.Features.Entities;
+using SmsHub.Domain.Features.Template.MediatorDtos.Commands;
+
+namespace SmsHub.Application.Features.Template.Mappings
+{
+    public class TemplateMapping:Profile
+    {
+        public TemplateMapping()
+        {
+            CreateMap<Entities.Template, CreateTemplateDto>().ReverseMap();
+        }
+    }
+}
