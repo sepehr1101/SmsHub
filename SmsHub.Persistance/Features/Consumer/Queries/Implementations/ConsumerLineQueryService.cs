85cc828a3157d1c0d4ca7b24351791239082c1eb
sepehr shamsaie <mantera.sh@hotmail.com>
2024-09-25 10:14:53 +0330

    Consumer Query Service

---
+++ SmsHub.Persistance/Features/Consumer/Queries/Implementations/ConsumerLineQueryService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities = SmsHub.Domain.Features.Entities;
+using SmsHub.Persistence.Contexts.UnitOfWork;
+using SmsHub.Common.Extensions;
+using SmsHub.Persistence.Features.Consumer.Queries.Contracts;
+
+namespace SmsHub.Persistence.Features.Consumer.Queries.Implementations
+{
+    public class ConsumerLineQueryService : IConsumerLineQueryService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly DbSet<Entities.Consumer> _consumers;
+        public ConsumerLineQueryService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _consumers = _uow.Set<Entities.Consumer>();
+        }
+        public async Task<Entities.Consumer> Get(int id)
+        {
+            var entity = await _consumers.FindAsync(id);
+            entity.NotNull(nameof(Entities.Consumer));
+            return entity;
+        }
+        public async Task<ICollection<Entities.Consumer>> Get()
+        {
+            var entities = await _consumers.AsNoTracking().ToListAsync();
+            entities.NotNull(nameof(Entities.Consumer));
+            return entities;
+        }
+    }
+}
