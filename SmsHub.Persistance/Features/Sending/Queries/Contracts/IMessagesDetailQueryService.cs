0ed6718bee6cadfda4c45b50b1a2b31e131fc3ec
ZEtehadi <zetehadi82@gmail.com>
2024-09-25 13:58:10 +0330

    set Persistence-Sending-Queries-Get

---
+++ SmsHub.Persistance/Features/Sending/Queries/Contracts/IMessagesDetailQueryService.cs
@@ -0,0 +1,8 @@
+﻿namespace SmsHub.Persistence.Features.Sending.Queries.Contracts
+{
+    public interface IMessagesDetailQueryService
+    {
+        Task<ICollection<Domain.Features.Entities.MessagesDetail>> Get();
+        Task<Domain.Features.Entities.MessagesDetail> Get(int id);
+    }
+}
---
+++ SmsHub.Persistance/Features/Sending/Queries/Implementations/MessagesDetailQueryService.cs
@@ -0,0 +1,32 @@
+﻿using Entities = SmsHub.Domain.Features.Entities;
+using SmsHub.Persistence.Features.Sending.Queries.Contracts;
+using Microsoft.EntityFrameworkCore;
+using SmsHub.Persistence.Contexts.UnitOfWork;
+using SmsHub.Common.Extensions;
+
+namespace SmsHub.Persistence.Features.Sending.Queries.Implementations
+{
+    public class MessagesDetailQueryService: IMessagesDetailQueryService
+    {
+
+        private readonly IUnitOfWork _uow;
+        private readonly DbSet<Entities.MessagesDetail> _messagesDetails;
+        public MessagesDetailQueryService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _messagesDetails = _uow.Set<Entities.MessagesDetail>();
+        }
+        public async Task<ICollection<Entities.MessagesDetail>> Get()
+        {
+            var entities = await _messagesDetails.AsNoTracking().ToListAsync();
+            entities.NotNull(nameof(Entities.MessagesDetail));
+            return entities;
+        }
+        public async Task<Entities.MessagesDetail> Get(int id)
+        {
+            var entity = await _messagesDetails.FindAsync(id);
+            entity.NotNull(nameof(Entities.MessagesDetail));
+            return entity;
+        }
+    }
+}
