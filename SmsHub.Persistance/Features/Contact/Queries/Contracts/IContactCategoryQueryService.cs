4029acaf404c5930a1383bdac75c8dd89a3118bb
ZEtehadi <zetehadi82@gmail.com>
2024-09-25 13:24:21 +0330

    set Persistence-contact-Queries-Get

---
+++ SmsHub.Persistance/Features/Contact/Queries/Contracts/IContactCategoryQueryService.cs
@@ -0,0 +1,8 @@
+﻿namespace SmsHub.Persistence.Features.Contact.Queries.Contracts
+{
+    public interface IContactCategoryQueryService
+    {
+        Task<ICollection<Domain.Features.Entities.ContactCategory>> Get();
+        Task<Domain.Features.Entities.ContactCategory> Get(int id);
+    }
+}
---
+++ SmsHub.Persistance/Features/Contact/Queries/Implementations/ContactCategoryQueryService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities= SmsHub.Domain.Features.Entities;
+using SmsHub.Persistence.Contexts.UnitOfWork;
+using SmsHub.Persistence.Features.Contact.Queries.Contracts;
+using SmsHub.Common.Extensions;
+
+namespace SmsHub.Persistence.Features.Contact.Queries.Implementations
+{
+    public class ContactCategoryQueryService: IContactCategoryQueryService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly DbSet<Entities.ContactCategory> _contactCategories;
+        public ContactCategoryQueryService(IUnitOfWork uow)
+        {
+            _uow=uow;
+            _contactCategories=_uow.Set<Entities.ContactCategory>();
+        }
+        public async Task<ICollection<Entities.ContactCategory>> Get()
+        {
+            var entities = await _contactCategories.AsNoTracking().ToListAsync();
+            entities.NotNull(nameof(Entities.ContactCategory));
+            return entities;
+        }
+        public async Task<Entities.ContactCategory> Get(int id)
+        {
+            var entity=await _contactCategories.FindAsync(id);
+            entity.NotNull(nameof(Entities.ContactCategory));
+            return entity;
+        }
+
+
+    }
+}
