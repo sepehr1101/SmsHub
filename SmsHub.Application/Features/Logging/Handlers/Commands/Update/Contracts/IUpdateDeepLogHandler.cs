77c433722c7cc9f33cfa80ca7dfa7876098e2399
ZEtehadi <zetehadi82@gmail.com>
2024-10-01 12:45:07 +0330

    Update Hndler/Mapping/Mediator for Logging

---
+++ SmsHub.Application/Features/Logging/Handlers/Commands/Update/Contracts/IUpdateDeepLogHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsHub.Application.Features.Logging.Handlers.Commands.Update.Contracts
+{
+    internal interface IUpdateDeepLogHandler
+    {
+    }
+}
