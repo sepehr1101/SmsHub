d36edb146913a96b1eaa330f2c5a30c6175d8c01
ZEtehadi <zetehadi82@gmail.com>
2024-10-07 12:06:33 +0330

    Create&Delete&Update Controller for Contact

---
+++ SmsHub.Api/Controllers/Contact/Commands/Create/ContactCategoryCreateController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmsHub.Application.Features.Contact.Handlers.Commands.Create.Contracts;
+using SmsHub.Common.Extensions;
+using SmsHub.Domain.Features.Contact.MediatorDtos.Commands.Create;
+using SmsHub.Persistence.Contexts.UnitOfWork;
+
+namespace SmsHub.Api.Controllers.Contact.Commands.Create
+{
+    [Route("api/ContactCategory")]
+    [ApiController]
+    public class ContactCategoryCreateController : ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly ICreateContactCategoryCommandHandler _createCommandHandler;
+        public ContactCategoryCreateController(IUnitOfWork uow, ICreateContactCategoryCommandHandler createCommandHandler)
+        {
+            _uow = uow;
+            _uow.NotNull(nameof(uow));
+
+            _createCommandHandler = createCommandHandler;
+            _createCommandHandler.NotNull(nameof(createCommandHandler));
+        }
+        [HttpGet(Name = nameof(Create))]
+        public async Task<IActionResult> Create([FromBody] CreateContactCategoryDto createDto, CancellationToken cancellationToken)
+        {
+            await _createCommandHandler.Handle(createDto, cancellationToken);
+            await _uow.SaveChangesAsync(cancellationToken);
+            return Ok(createDto);
+        }
+    }
+}
---
+++ SmsHub.Api/Controllers/Contact/Commands/Delete/ContactNumberCategoryDeleteController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmsHub.Application.Features.Contact.Handlers.Commands.Delete.Contracts;
+using SmsHub.Common.Extensions;
+using SmsHub.Domain.Features.Contact.MediatorDtos.Commands.Delete;
+using SmsHub.Persistence.Contexts.UnitOfWork;
+
+namespace SmsHub.Api.Controllers.Contact.Commands.Delete
+{
+    [Route("api/ContactNumberCategory")]
+    [ApiController]
+    public class ContactNumberCategoryDeleteController : ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IContactNumberCategoryDeleteHandler _deleteCommandHandler;
+        public ContactNumberCategoryDeleteController(IUnitOfWork uow, IContactNumberCategoryDeleteHandler deleteCommandHandler)
+        {
+            _uow = uow;
+            _uow.NotNull(nameof(uow));
+
+            _deleteCommandHandler = deleteCommandHandler;
+            _deleteCommandHandler.NotNull(nameof(deleteCommandHandler));
+        }
+        [HttpGet(Name = nameof(Delete))]
+        public async Task<IActionResult> Delete([FromBody] DeleteContactNumberCategoryDto deleteDto, CancellationToken cancellationToken)
+        {
+            await _deleteCommandHandler.Handle(deleteDto, cancellationToken);
+            await _uow.SaveChangesAsync(cancellationToken);
+            return Ok();
+        }
+    }
+}
