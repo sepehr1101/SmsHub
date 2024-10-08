885fc7aebb85a4914b0c3d14a2f45a79c008426a
ZEtehadi <zetehadi82@gmail.com>
2024-10-08 12:59:47 +0330

    -

--- SmsHub.Domain/Features/Contact/MediatorDtos/Commands/Create/CreateContactCategoryDto.cs
+++ SmsHub.Domain/Features/Contact/MediatorDtos/Commands/Create/CreateContactCategoryDto.cs
@@ -1,6 +1,8 @@
-﻿namespace SmsHub.Domain.Features.Contact.MediatorDtos.Commands.Create
+﻿using MediatR;
+
+namespace SmsHub.Domain.Features.Contact.MediatorDtos.Commands.Create
 {
-    public record CreateContactCategoryDto
+    public record CreateContactCategoryDto: IRequest
     {
         public string? Title { get; init; }
         public string Description { get; init; }
