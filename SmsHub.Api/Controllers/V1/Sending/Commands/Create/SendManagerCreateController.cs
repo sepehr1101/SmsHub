77b8cd4f48fba148e4f4614cd030ed2540d33ff0
ZEtehadi <zetehadi82@gmail.com>
2024-12-16 13:16:38 +0330

    MessageBatchFacory sample

--- SmsHub.Api/Controllers/V1/Sending/Commands/Create/SendManagerCreateController.cs
+++ SmsHub.Api/Controllers/V1/Sending/Commands/Create/SendManagerCreateController.cs
@@ -1,8 +1,10 @@
 ﻿using Aban360.Api.Controllers.V1;
 using Microsoft.AspNetCore.Mvc;
 using SmsHub.Application.Features.Sending.Handlers.Commands.Create.Contracts;
+using SmsHub.Application.Features.Sending.Services;
 using SmsHub.Application.Features.Template.Handlers.Queries.Contracts;
 using SmsHub.Common.Extensions;
+using SmsHub.Domain.Features.Sending.MediatorDtos.Commands.Create;
 using SmsHub.Persistence.Contexts.UnitOfWork;
 
 namespace SmsHub.Api.Controllers.V1.Sending.Commands.Create
@@ -32,8 +34,16 @@ namespace SmsHub.Api.Controllers.V1.Sending.Commands.Create
         [Route("SendManager/{templateId}/{lineId}")]
         public async Task<IActionResult> SendManager(int templateId,int lineId)
         {
-            await _sendManagerCreateHandler.Handle(templateId,lineId, new CancellationToken());
-            return Ok(templateId);
+           var mobileText= await _sendManagerCreateHandler.Handle(templateId,lineId, new CancellationToken());
+            return Ok(mobileText);
+        }
+
+        [HttpPost]
+        [Route("x")]
+        public async Task<IActionResult> x(ICollection<MobileText> mobileText)
+        {
+            var s= MessageBatchFactory.Create(mobileText,1,"one");
+            return Ok(s);
         }
 
     }
