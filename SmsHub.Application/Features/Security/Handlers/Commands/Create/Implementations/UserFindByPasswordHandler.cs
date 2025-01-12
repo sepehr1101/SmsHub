c0892c3717ed98695541a8c15ddbdfe315b0f5d3
ZEtehadi <zetehadi82@gmail.com>
2025-01-12 16:31:48 +0330

    FirstStep

--- SmsHub.Application/Features/Security/Handlers/Commands/Create/Implementations/UserFindByPasswordHandler.cs
+++ SmsHub.Application/Features/Security/Handlers/Commands/Create/Implementations/UserFindByPasswordHandler.cs
@@ -6,7 +6,6 @@ using SmsHub.Domain.Features.Security.Dtos;
 using SmsHub.Domain.Features.Security.Entities;
 using SmsHub.Persistence.Features.Security.Commands.Contracts;
 using SmsHub.Persistence.Features.Security.Queries.Contracts;
-using System.Net.Http;
 
 namespace SmsHub.Application.Features.Security.Handlers.Commands.Create.Implementations
 {
@@ -37,82 +36,26 @@ namespace SmsHub.Application.Features.Security.Handlers.Commands.Create.Implemen
 
             if (user == null)
             {
-                 await GetUserLogin(InvalidLoginReasonEnum.InvalidUsername, false, false, input,user);
-                //var userLogin = new UserLogin()
-                //{
-                //    Id = new Guid(),
-                //    FirstStepSuccess = false,
-                //    AppVersion = input.AppVersion,
-                //    FirstStepDateTime = DateTime.Now,
-                //    LogInfo = logInfoString,
-                //    Ip = _contextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString(),
-                //    InvalidLoginReasonId = InvalidLoginReasonEnum.InvalidUsername,
-                //};
-                //await _userLoginCommandService.Add(userLogin);
-
+                await GetUserLogin(InvalidLoginReasonEnum.InvalidUsername, false, false, input, user);
                 return (user, false);
 
             }
-            else//user is not null
+            else
             {
                 var hashedPassword = await SecurityOperations.GetSha512Hash(input.Password);
                 if (hashedPassword != user.Password)
                 {
-                     await GetUserLogin(InvalidLoginReasonEnum.InvalidPassword, true, true, input,user);
-                    //var userLogin = new UserLogin()
-                    //{
-                    //    Id = new Guid(),
-                    //    Username = input.Username,
-                    //    WrongPassword = input.Password,
-                    //    FirstStepSuccess = false,
-                    //    AppVersion = input.AppVersion,
-                    //    FirstStepDateTime = DateTime.Now,
-                    //    LogInfo = logInfoString,
-                    //    Ip = _contextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString(),
-                    //    InvalidLoginReasonId = InvalidLoginReasonEnum.InvalidPassword,
-                    //};
-                    //await _userLoginCommandService.Add(userLogin);
-
+                    await GetUserLogin(InvalidLoginReasonEnum.InvalidPassword, true, true, input, user);
                     return (user, false);
                 }
-                else if (user.InvalidLoginAttemptCount > 3)//UserName , Password valid --> LockedUser
+                else if (user.InvalidLoginAttemptCount > 3)//todo: check
                 {
-                    await GetUserLogin(InvalidLoginReasonEnum.LockedUser, true, true, input, user);//todo: FirstStepSuccess false|true
-                    //var userLogin = new UserLogin()
-                    //{
-                    //    Id = new Guid(),
-                    //    UserId=user.Id,
-                    //    Username = input.Username,
-                    //    WrongPassword = input.Password,
-                    //    FirstStepSuccess = false,//todo : true or not??
-                    //    AppVersion = input.AppVersion,
-                    //    FirstStepDateTime = DateTime.Now,
-                    //    LogInfo = logInfoString,
-                    //    Ip = _contextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString(),
-                    //    InvalidLoginReasonId =InvalidLoginReasonEnum.LockedUser,
-                    //};
-                    //await _userLoginCommandService.Add(userLogin);
-
+                    await GetUserLogin(InvalidLoginReasonEnum.LockedUser, true, true, input, user);
                     return (user, false);
                 }
-                else if (user.LockTimespan > DateTime.Now)//Inactive User
+                else if (user.LockTimespan > DateTime.Now)//todo: check
                 {
-                    await GetUserLogin(InvalidLoginReasonEnum.InactiveUser, true, true, input, user);//todo: FirstStepSuccess false|true
-                    //var userLogin = new UserLogin()
-                    //{
-                    //    Id = new Guid(),
-                    //    UserId = user.Id,
-                    //    Username = input.Username,
-                    //    WrongPassword = input.Password,
-                    //    FirstStepSuccess = false,//todo : true or not??
-                    //    AppVersion = input.AppVersion,
-                    //    FirstStepDateTime = DateTime.Now,
-                    //    LogInfo = logInfoString,
-                    //    Ip = _contextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString(),
-                    //    InvalidLoginReasonId = InvalidLoginReasonEnum.InactiveUser,
-                    //};
-                    //await _userLoginCommandService.Add(userLogin);
-
+                    await GetUserLogin(InvalidLoginReasonEnum.InactiveUser, true, true, input, user);
                     return (user, false);
                 }
                 else//valid
@@ -124,7 +67,7 @@ namespace SmsHub.Application.Features.Security.Handlers.Commands.Create.Implemen
             }
 
 
-       
+
 
         }
         public string GetLogInfo()
@@ -135,20 +78,21 @@ namespace SmsHub.Application.Features.Security.Handlers.Commands.Create.Implemen
             return logInfoString;
         }
 
-        public async Task GetUserLogin(InvalidLoginReasonEnum LoginReasonEnum, bool IsUserName, bool IsPassword, FirstStepLoginInput input,User? user)
+        private async Task GetUserLogin(InvalidLoginReasonEnum LoginReasonEnum, bool IsUserName, bool IsPassword, FirstStepLoginInput input,
+            User? user)
         {
             var userLogin = new UserLogin()
             {
                 Id = new Guid(),
-                Username=IsUserName? input.Username : null,
-                WrongPassword=IsPassword? input.Password : null,
-                UserId=user!=null? user.Id :null,//todo: true or not?
+                Username = IsUserName ? input.Username : null,
+                WrongPassword = IsPassword ? input.Password : null,
+                UserId = user != null ? user.Id : null,
                 FirstStepSuccess = false,
                 AppVersion = input.AppVersion,
                 FirstStepDateTime = DateTime.Now,
                 LogInfo = GetLogInfo(),
                 Ip = _contextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString(),
-                InvalidLoginReasonId =LoginReasonEnum,
+                InvalidLoginReasonId = LoginReasonEnum,
             };
             await _userLoginCommandService.Add(userLogin);
         }
