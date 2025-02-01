d9cf4e5011bd3532698aca45b52835700a053177
ZEtehadi <zetehadi82@gmail.com>
2025-02-01 17:44:34 +0330

    userLine

--- SmsHub.Application/Features/Security/Handlers/Commands/Create/Implementations/UserLineCreateHandler.cs
+++ SmsHub.Application/Features/Security/Handlers/Commands/Create/Implementations/UserLineCreateHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using SmsHub.Application.Exceptions;
 using SmsHub.Application.Features.Security.Handlers.Commands.Create.Contracts;
 using SmsHub.Common.Extensions;
 using SmsHub.Domain.Features.Security.Dtos;
@@ -15,11 +16,13 @@ namespace SmsHub.Application.Features.Security.Handlers.Commands.Create.Implemen
         private readonly IMapper _mapper;
         private readonly IUserQueryService _userQueryService;
         private readonly ILineQueryService _lineQueryService;
+        private readonly IUserLineQueryService _userLineQueryService;
         public UserLineCreateHandler(
             IUserLineCommandService userLineCommandService,
             IMapper mapper,
             IUserQueryService userQueryService,
-            ILineQueryService lineQueryService)
+            ILineQueryService lineQueryService,
+            IUserLineQueryService userLineQueryService)
         {
             _userLineCommandService = userLineCommandService;
             _userLineCommandService.NotNull(nameof(userLineCommandService));
@@ -30,14 +33,23 @@ namespace SmsHub.Application.Features.Security.Handlers.Commands.Create.Implemen
             _userQueryService = userQueryService;
             _userQueryService.NotNull(nameof(userQueryService));
 
-            _lineQueryService = lineQueryService; 
-            _lineQueryService.NotNull(nameof(lineQueryService));    
+            _lineQueryService = lineQueryService;
+            _lineQueryService.NotNull(nameof(lineQueryService));
+
+            _userLineQueryService = userLineQueryService;
+            _userLineQueryService.NotNull(nameof(userLineQueryService));
         }
         public async Task Handle(CreateUserLineDto createUserLineDto,CancellationToken cancellationToken)
         {
             var user=await _userQueryService.Get(createUserLineDto.UserId);
             var line=await _lineQueryService.Get(createUserLineDto.LineId);
 
+            var anyUserLine = await _userLineQueryService.Any(createUserLineDto.LineId, createUserLineDto.UserId);
+
+            if (anyUserLine)
+            {
+                throw new DuplicateItemException();
+            }
             var userLine = _mapper.Map<UserLine>(createUserLineDto);
             await _userLineCommandService.Add(userLine);
         }
